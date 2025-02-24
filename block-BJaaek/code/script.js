e9fd47d4f805bf8950bb6230a3d3c7b7239a16f9
= <=>
2025-02-24 17:18:15 -0500

    dsfs

--- block-BJaaek/code/script.js
+++ block-BJaaek/code/script.js
@@ -1,28 +1,28 @@
-let buttonone = document.querySelector(".first");
-let buttontwo = document.querySelector(".second");
+let boxone = document.querySelector(".first");
+let boxtwo = document.querySelector(".second");
+
 
 function generaterandomcolor(){
-    let hexcharacters = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
-
-
-    let color = "#";
-    for(let i=0;i<6;i++){
-    let randomNumber = Math.floor(Math.random() * 16);
-    
-    color = color + hexcharacters[randomNumber]
-    }
-    return color;
+let hexcharacters = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+let color = "#";
+for( let i=0;i<6;i++){
+    let randomnumber = Math.floor(Math.random() * 16);
+    color = color + hexcharacters[randomnumber];
 }
-
+return color;
+}
 
 function handleclick(){
-let randomcolor = generaterandomcolor();
-buttonone.style.backgroundcolor = randomcolor;
-}
-function handlemousemove(){
-let randomcolor = generaterandomcolor();
-buttontwo.style.backgroundcolor = randomcolor;
+    /* 1.Generate random color
+       2.Change the background color of the box */
+       let randomcolor = generaterandomcolor();
+boxone.style.backgroundColor = randomcolor;
 }
 
-buttonone.addEventListener("click",handleclick);
-buttontwo.addEventListener("mousemove",handlemousemove);
+function handlemousemove(){
+    let randomcolor = generaterandomcolor();
+    boxtwo.style.backgroundColor = randomcolor;
+}
+boxone.addEventListener("click", handleclick);
+boxtwo.addEventListener("mousemove", handlemousemove);
+
